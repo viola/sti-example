f94b605ff3596db9c18cd72ed160739e48386bf1
Viola Holownia <violaholownia@viola-holownias-macbook.local>
2010-03-03 20:39:46 -0500

    add README and models annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
   has_many :addresses, :dependent => :delete_all
   has_one  :home_address
--- app/models/work_address.rb
+++ app/models/work_address.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id         :integer         not null, primary key
+#  street     :string(255)
+#  city       :string(255)
+#  state      :string(255)
+#  postal     :string(255)
+#  country    :string(255)
+#  type       :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class WorkAddress < Address
 end
